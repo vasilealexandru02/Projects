e49fb50a722eb2678287de5732757d16e5f0bb09
Vasile Alexandru <73804089+vasilealexandru02@users.noreply.github.com>
2023-07-12 16:26:14 +0200

    Hecha la logica de tres en raya diagonal

--- Tic Tac Toe/main.js
+++ Tic Tac Toe/main.js
@@ -1,96 +1,96 @@
-var generatedNumber;
-var letters = ["X", "0"];
-var letterSelected;
-var gameboard;
-function playGame() {
+var generatedNumber
+var letters = ['X', '0']
+var letterSelected
+var gameboard
+function playGame () {
   gameboard = [
-    ["Z", "Z", "Z"],
-    ["Z", "Z", "Z"],
-    ["Z", "Z", "Z"],
-  ];
+    ['Z', 'Z', 'Z'],
+    ['Z', 'Z', 'Z'],
+    ['Z', 'Z', 'Z']
+  ]
   // debugger;
-  var gameboards = document.getElementsByClassName("gameboard");
+  var gameboards = document.getElementsByClassName('gameboard')
   for (var i = 0; i < gameboards.length; i++) {
-    gameboards[i].innerHTML = "";
+    gameboards[i].innerHTML = ''
   }
-  generateLetter();
+  generateLetter()
   document.getElementById(
-    "gameText"
-  ).innerHTML = `${letterSelected} is starting!`;
+    'gameText'
+  ).innerHTML = `${letterSelected} is starting!`
 }
 
-function generateLetter() {
-  let generatedNumber = Math.floor(Math.random() * 2);
-  letterSelected = letters[generatedNumber];
+function generateLetter () {
+  let generatedNumber = Math.floor(Math.random() * 2)
+  letterSelected = letters[generatedNumber]
 }
 
-function switchLetter() {
+function switchLetter () {
   // debugger;
   if (letterSelected == letters[0]) {
-    letterSelected = letters[1];
+    letterSelected = letters[1]
   } else {
-    letterSelected = letters[0];
+    letterSelected = letters[0]
   }
   document.getElementById(
-    "gameText"
-  ).innerHTML = `${letterSelected} player turn!`;
+    'gameText'
+  ).innerHTML = `${letterSelected} player turn!`
 }
 
-function clickedSquare(e) {
-  let clickedSquare = document.getElementById(e.target.id);
+function clickedSquare (e) {
+  let clickedSquare = document.getElementById(e.target.id)
 
   // // Apply disabled style
   // clickedSquare.style.opacity = "0.5"; // For visual indication (optional)
   // clickedSquare.style.pointerEvents = "none"; // Disable pointer events (op
-  if (clickedSquare.innerHTML == "") {
+  if (clickedSquare.innerHTML == '') {
     if (letterSelected != undefined) {
-      clickedSquare.innerHTML = `<p class="text-center fs-1 align-middle"  id="${e.target.id}}-text">${letterSelected}</p>`;
-      clickedSquare.disabled = true; // Set the disabled attribute to true
-      let clickedSquareId = e.target.id;
-      insertIntoGameboard(clickedSquareId);
-      console.log(checkBoard(letterSelected));
+      clickedSquare.innerHTML = `<p class="text-center fs-1 align-middle"  id="${e.target.id}}-text">${letterSelected}</p>`
+      clickedSquare.disabled = true // Set the disabled attribute to true
+      let clickedSquareId = e.target.id
+      insertIntoGameboard(clickedSquareId)
+      console.log(checkBoard(letterSelected))
     }
-    console.log(gameboard);
-    switchLetter();
+    console.log(gameboard)
+    switchLetter()
   }
 }
 
-function insertIntoGameboard(position) {
+function insertIntoGameboard (position) {
   switch (position) {
-    case "0":
-      gameboard[0][0] = letterSelected;
-      break;
-    case "1":
-      gameboard[0][1] = letterSelected;
-      break;
-    case "2":
-      gameboard[0][2] = letterSelected;
-      break;
-    case "3":
-      gameboard[1][0] = letterSelected;
-      break;
-    case "4":
-      gameboard[1][1] = letterSelected;
-      break;
-    case "5":
-      gameboard[1][2] = letterSelected;
-      break;
-    case "6":
-      gameboard[2][0] = letterSelected;
-      break;
-    case "7":
-      gameboard[2][1] = letterSelected;
-      break;
-    case "8":
-      gameboard[2][2] = letterSelected;
-      break;
+    case '0':
+      gameboard[0][0] = letterSelected
+      break
+    case '1':
+      gameboard[0][1] = letterSelected
+      break
+    case '2':
+      gameboard[0][2] = letterSelected
+      break
+    case '3':
+      gameboard[1][0] = letterSelected
+      break
+    case '4':
+      gameboard[1][1] = letterSelected
+      break
+    case '5':
+      gameboard[1][2] = letterSelected
+      break
+    case '6':
+      gameboard[2][0] = letterSelected
+      break
+    case '7':
+      gameboard[2][1] = letterSelected
+      break
+    case '8':
+      gameboard[2][2] = letterSelected
+      break
   }
 }
 
-function checkBoard(lastLetter) {
+function checkBoard (lastLetter) {
   // Obtener el número de filas y columnas
-  const filas = gameboard.length;
-  const columnas = gameboard[0].length;
+  const filas = gameboard.length
+  const columnas = gameboard[0].length
 
   // if(matriz[0][0]==letterSelected && matriz[0][1]==letterSelected && matriz[0][2]==letterSelected){
   //   console.log('Player', letterSelected, 'won!')
@@ -103,9 +103,9 @@ function checkBoard(lastLetter) {
         gameboard[i][j + 1] == letterSelected &&
         gameboard[i][j + 2] == letterSelected
       ) {
-        return true;
+        return true
       }
-      j = 3;
+      j = 3
     }
   }
   // Second iteration over the columns
@@ -120,39 +120,60 @@ function checkBoard(lastLetter) {
         gameboard[i + 1][j] == letterSelected &&
         gameboard[i + 2][j] == letterSelected
       ) {
-        return true;
+        return true
       }
-      i = 3;
+      i = 3
     }
   }
   // Iteration over diagonals
-  // Left to right diagonal
-  console.log("diagonal izquierda derecha");
+  // Right to left diagonal
+  console.log('Diagonal derecha a izquierda')
   for (let k = 0; k < filas + columnas - 1; k++) {
-    let inicioFila = Math.max(0, k - columnas + 1);
-    let inicioColumna = Math.min(k, columnas - 1);
+    let inicioFila = Math.max(0, k - columnas + 1)
+    let inicioColumna = Math.min(k, columnas - 1)
     for (let i = inicioFila, j = inicioColumna; i < filas && j >= 0; i++, j--) {
       if (k == 2) {
-        console.log(gameboard[i][j]);
+        // console.log('x', i, 'y', j);
+        // console.log(gameboard[i][j]);
+        // console.log(gameboard[i+1][j-1]);
+        // console.log(gameboard[i+2][j-2]);
+        // console.log(gameboard[i][j]);
+        if (
+          gameboard[i][j] == letterSelected &&
+          gameboard[i + 1][j - 1] == letterSelected &&
+          gameboard[i + 2][j - 2] == letterSelected
+        ) {
+          console.log('diagonal derecha izquierda tres en raya!')
+        }
+        i = 3
+        j = 0
       }
       //console.log(gameboard[i][j]);
     }
   }
-  if (letterMatch == 3) {
-    return true;
-  }
 
-  console.log("diagonal derecha izquierda");
-  // Iterar sobre la matriz en diagonal de derecha a izquierda
+  console.log('Diagonal izquierda derecha')
+  // Left to right diagonal
   for (let k = 0; k < filas + columnas - 1; k++) {
-    let inicioFila = Math.max(0, k - columnas + 1);
-    let inicioColumna = Math.min(k, columnas - 1);
+    let inicioFila = Math.max(0, k - columnas + 1)
+    let inicioColumna = Math.min(k, columnas - 1)
     for (let i = inicioFila, j = inicioColumna; i < filas && j >= 0; i++, j--) {
       if (k == 2) {
-        console.log(gameboard[i][columnas - 1 - j]);
+        if (
+          gameboard[i][columnas - 1 - j] == letterSelected &&
+          gameboard[i + 1][columnas - 1 - j + 1] == letterSelected &&
+          gameboard[i + 2][columnas - 1 - j + 2] == letterSelected
+        ) {
+          console.log('diagonal izquierda a derecha tres en raya!')
+        }
+        // console.log(gameboard[i][columnas - 1 - j]);
+        // console.log(gameboard[i+1][columnas - 1 - j+1]);
+        // console.log(gameboard[i+2][columnas - 1 - j+2]);
+        i = 3
+        j = 0
       }
     }
   }
 
-  return false;
+  return false
 }
