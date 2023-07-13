752ed2990cfcd10894a49ffe018cfd1486b2708b
Vasile Alexandru <73804089+vasilealexandru02@users.noreply.github.com>
2023-07-13 19:06:30 +0200

    Hecha petición a API Hecho mostrado de resultados

--- WeatherAPI/main.js
+++ WeatherAPI/main.js
@@ -1,1 +1,38 @@
-console.log(`prueba`)
+const imageUrl =
+  'https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/PNG/2nd%20Set%20-%20Monochrome/'
+
+function searchCityWeather () {
+  var cityName = document.getElementById('cityName').value
+  const cityinfo = fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?unitGroup=uk&include=days&key=ANAAWVC9KK34JGFSLJVLQNNPG&contentType=json`,
+    {
+      method: 'GET',
+      headers: {}
+    }
+  )
+    .then(response => response.json())
+    .then(city => {
+      document.getElementById('msg').innerHTML = ''
+      return city
+    })
+    .catch(err => {
+      document.getElementById('msg').innerHTML =
+        'The city introduced is not valid! 😣!'
+    })
+
+  const printCity = () => {
+    cityinfo.then(a => {
+      debugger
+      var resultsContainer = document.getElementById('results')
+      resultsContainer.innerHTML += `<div  class="cityName rounded w-33">
+          <p class="lightBlue">${a.resolvedAddress.split(',')[0]}</p>
+          <p>${a.days[0].temp} ºC</p>
+          <img src="${imageUrl}${a.days[0].icon}.png" />
+          <p class="lightBlue">${a.days[0].conditions}</p>
+        </div>`
+      console.log(a)
+    })
+  }
+  printCity()
+  var cityName = (document.getElementById('cityName').value = '')
+}
