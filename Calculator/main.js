f4182576d705de5317d2ea91890a6a8d85530ace
Vasile Alexandru <73804089+vasilealexandru02@users.noreply.github.com>
2023-07-24 14:53:18 +0200

    Refactor main.js

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,6 +1,4 @@
 document.addEventListener("click", (event) => {
-  console.log(event.target.id);
-
   if (
     event.target.id != "" &&
     event.target.id != "calculator" &&
@@ -12,28 +10,26 @@ document.addEventListener("click", (event) => {
     // Displays into the input element the pressed button
     calculatorInput.innerHTML += event.target.innerHTML;
     clearButton(event.target.innerHTML);
-    // console.log(event.target.innerHTML);
     calculateResult(event.target.id);
   }
 });
 
+// Clears user's input
 const clearButton = (clickedButton) => {
-  console.log("clickedButton", clickedButton);
   if (clickedButton == "C") {
     document.getElementById("input").innerText = "0";
   }
 };
 
+// Method that calculates the result when the equal button is clicked
 const calculateResult = (clickedButton) => {
   try {
     if (clickedButton == "equal-button") {
       let calculatorInput = document.getElementById("input").innerText;
 
-      const prueba = calculatorInput.substring(0, calculatorInput.length - 1);
-      console.log(prueba);
-      document.getElementById("input").innerText = eval(prueba).toFixed(2);
-
-      // console.log(eval(prueba));
+      const input = calculatorInput.substring(0, calculatorInput.length - 1);
+      console.log(input);
+      document.getElementById("input").innerText = eval(input).toFixed(2);
     }
   } catch (error) {
     document.getElementById("input").innerText = "";
