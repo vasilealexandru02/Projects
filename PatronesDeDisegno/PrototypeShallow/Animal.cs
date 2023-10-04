7d80ecf632cf5bc924da8d44d774102527910d48
Vasile Alexandru <73804089+vasilealexandru02@users.noreply.github.com>
2023-10-04 14:15:35 +0200

    Prototype shallow cloning

---
+++ PatronesDeDisegno/PrototypeShallow/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeShallow
+{
+    public class Animal : ICloneable
+    {
+        public int Patas { get; set; }
+
+        public string Nombre { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+
+        // Clonación superficial 
+
+
+    }
+}
---
+++ PatronesDeDisegno/PrototypeShallow/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PrototypeShallow;
+
+Animal animal = new Animal()
+{
+    Nombre = "Oveja",
+    Patas = 4
+};
+// En este caso si lo dejamos así, lo que está haciendo es tener como referencia el primer objeto.
+
+
+//Animal animalClonado = animal;
+// Al clonarlo, sí que se están tratanto dos objetos
+
+Animal animalClonado = animal.Clone() as Animal;
+
+// Si modifico este segundo objeto asignándole 5 patas, en realidad estoy modificando el primero, porque es la misma posición de memoria
+animalClonado.Patas = 5;
+
+Console.WriteLine("Nº de patas animal original: " + animal.Patas);
+Console.WriteLine("Nº de patas animal clonado: " + animalClonado.Patas);
+
