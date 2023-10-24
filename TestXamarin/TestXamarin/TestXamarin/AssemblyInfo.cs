ffa57e9aa3260000b19be0947dea9b2ceb213639
Vasile Alexandru <73804089+vasilealexandru02@users.noreply.github.com>
2023-10-24 14:38:36 +0200

    Examples adding new fonts to the project and using them as fontimagesource and text. Added a static class with the unicode values.

--- TestXamarin/TestXamarin/TestXamarin/AssemblyInfo.cs
+++ TestXamarin/TestXamarin/TestXamarin/AssemblyInfo.cs
@@ -1,3 +1,10 @@
+using System.Reflection;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+
+[assembly: ExportFont("Font Awesome 6 Brands-Regular-400.otf", Alias = "RegularBrands")]
+[assembly: ExportFont("Font Awesome 6 Free-Regular-400.otf", Alias = "RegularFree")]
+[assembly: ExportFont("Font Awesome 6 Free-Solid-900.otf", Alias = "SolidFree")]
